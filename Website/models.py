f80ed359ba9ff28505ff373aec14a33a232ba1fc
Szaneron <szaneron@gmail.com>
2023-08-20 20:28:29 +0200

    added delivery module with correct display of data on the main panel

--- Website/models.py
+++ Website/models.py
@@ -6,22 +6,29 @@ from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
     """
-        Model representing user profiles in the application.
+    Model representing user profiles in the application.
 
-        Fields:
-        - user (OneToOneField): One-to-one relationship with the built-in User model.
-        - profile_picture (ImageField, optional): Profile picture of the user.
-        - assigned_tasks (PositiveIntegerField, default=0): Number of assigned tasks.
-        - completed_tasks (PositiveIntegerField, default=0): Number of completed tasks.
+    Fields:
+    - user (OneToOneField): One-to-one relationship with the built-in User model.
+    - profile_picture (ImageField, optional): Profile picture of the user.
+    - assigned_tasks (PositiveIntegerField, default=0): Number of assigned tasks.
+    - completed_tasks (PositiveIntegerField, default=0): Number of completed tasks.
+    - position (CharField): User's position, choices: "Pracownik" or "Szef".
 
-        Methods:
-        - __str__: Returns the string representation of the user profile.
+    Methods:
+    - __str__: Returns the string representation of the user profile.
 
-        """
+    """
+
+    POSITION_CHOICES = [
+        ('Pracownik', 'Pracownik'),
+        ('Szef', 'Szef'),
+    ]
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     profile_picture = models.ImageField(upload_to='profile_pictures', blank=True, null=True)
     assigned_tasks = models.PositiveIntegerField(default=0)
     completed_tasks = models.PositiveIntegerField(default=0)
+    position = models.CharField(max_length=10, choices=POSITION_CHOICES, default='Pracownik')
 
     def __str__(self):
         return self.user.username
@@ -44,6 +51,7 @@ class Task(models.Model):
     - __str__: Returns the string representation of the task.
 
     """
+
     STATUS_CHOICES = [
         ('Do zrobienia', 'Do zrobienia'),
         ('Zrobione', 'Zrobione'),
@@ -59,3 +67,53 @@ class Task(models.Model):
 
     def __str__(self):
         return f"{self.name} - {self.date}"
+
+
+class Delivery(models.Model):
+    """
+    Model representing delivery information.
+
+    Fields:
+    - delivery_company (CharField): Name of the delivery company, choices: "InPost", "DPD", "GLS", "Poczta", "DHL", "Media Expert".
+    - form (CharField): Form of the delivery, choices: "Paczka", "Paleta".
+    - quantity (IntegerField): Quantity of items.
+    - description (TextField): Description of the delivery.
+    - status (CharField): Status of the delivery, choices: "W drodze", "Odebrana", "Nie dostarczona".
+    - date (DateField): Date of the delivery.
+    - creation_time (TimeField): Time when the delivery was created.
+
+    Methods:
+    - __str__: Returns the string representation of the delivery.
+
+    """
+
+    DELIVERY_COMPANY_CHOICES = [
+        ('InPost', 'InPost'),
+        ('DPD', 'DPD'),
+        ('GLS', 'GLS'),
+        ('Poczta', 'Poczta'),
+        ('DHL', 'DHL'),
+        ('Media Expert', 'Media Expert'),
+    ]
+
+    FORM_CHOICES = [
+        ('Paczka', 'Paczka'),
+        ('Paleta', 'Paleta'),
+    ]
+
+    STATUS_CHOICES = [
+        ('W drodze', 'W drodze'),
+        ('Odebrana', 'Odebrana'),
+        ('Nie dostarczona', 'Nie dostarczona'),
+    ]
+
+    delivery_company = models.CharField(max_length=15, choices=DELIVERY_COMPANY_CHOICES)
+    form = models.CharField(max_length=10, choices=FORM_CHOICES, default='Paczka')
+    quantity = models.PositiveIntegerField()
+    description = models.TextField()
+    status = models.CharField(max_length=15, choices=STATUS_CHOICES, default='W drodze')
+    date = models.DateField()
+    creation_time = models.TimeField(default=datetime.time)
+
+    def __str__(self):
+        return f"{self.delivery_company} - {self.date}"
--- Website/views.py
+++ Website/views.py
@@ -1,17 +1,13 @@
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-from django.core.serializers import serialize
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from datetime import datetime
-from datetime import timedelta
-from .models import Task
+from .models import Task, Delivery
 
 
 def get_current_date():
     current_date = datetime.now()
-    # today = datetime.today()
-    # yesterday = today - timedelta(days=1)
     return current_date
 
 
@@ -40,14 +36,46 @@ def logout_user(request):
 def dashboard(request):
     user = request.user
     current_date = get_current_date()
-    print(user.userprofile.profile_picture.url)
     day_name = current_date.strftime('%A')
-    tasks = Task.objects.filter(date=current_date).order_by('creation_time')
+    all_tasks = Task.objects.filter(date=current_date).order_by('status', '-is_important', 'creation_time')
+    tasks_for_dashboard = all_tasks[:5]
+
+    def get_employee_rating():
+        """
+            Calculate the user's rating based on completed and assigned tasks.
+
+            This function calculates the user's rating based on the ratio of completed tasks
+            to assigned tasks. The rating is scaled to a range from 1 to 5.
+
+            Args:
+                user.userprofile (UserProfile): The user's profile containing task information.
+
+            Returns:
+                float: The calculated user's rating.
+
+            """
+        completed_tasks = user.userprofile.completed_tasks
+        assigned_tasks = user.userprofile.assigned_tasks
+
+        if assigned_tasks == 0:
+            return 'Brak zadań'
+
+        rating = min(5, max(1, completed_tasks / assigned_tasks * 5))
+        return round(rating, 1)
+
+    user_rating = get_employee_rating()
+
+    all_delivery = Delivery.objects.filter(date=current_date).order_by('-status', 'creation_time')
+    delivery_for_dashboard = all_delivery[:5]
 
     context = {
         'user': user,
         'current_date': current_date,
         'day_name': day_name,
-        'tasks': tasks,
+        'all_tasks': all_tasks,
+        'tasks_for_dashboard': tasks_for_dashboard,
+        'user_rating': user_rating,
+        'all_delivery': all_delivery,
+        'delivery_for_dashboard': delivery_for_dashboard
     }
     return render(request, "dashboard.html", context)
