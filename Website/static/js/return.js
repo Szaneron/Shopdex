935ef9bd46a705e671772a2504556211cc5ec821
Szaneron <szaneron@gmail.com>
2023-09-11 13:08:26 +0200

    added responsiveness for returns view

--- Website/static/js/return.js
+++ Website/static/js/return.js
@@ -1,2 +1,16 @@
 const active_link = document.getElementById('link_return')
 active_link.className = 'active'
+
+const sideMenu = document.querySelector('aside');
+const menuBtn = document.getElementById('menu-btn');
+const closeBtn = document.getElementById('close-btn');
+
+// Add a click event listener to the menu button to display the side menu.
+menuBtn.addEventListener('click', () => {
+    sideMenu.style.display = 'block';
+});
+
+// Add a click event listener to the close button to hide the side menu.
+closeBtn.addEventListener('click', () => {
+    sideMenu.style.display = '';
+});
